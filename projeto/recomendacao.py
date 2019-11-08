ade318ae58c44f1f568b5ef698f368e39c342be5
Matheus Abreu <matheusa596@gmail.com>
2019-11-07 21:12:23 -0300

    Otimização do MSD.

--- projeto/recomendacao.py
+++ projeto/recomendacao.py
@@ -2,23 +2,6 @@ from graph_tool.all import load_graph, is_bipartite, adjacency, vertex_similarit
 from scipy.sparse import csr_matrix
 import numpy as np
 
-# calcula a similaridade de um vértice para todos os outros do mesmo grupo que ele
-def sim_i_vertices(g, i, termos):
-	row = np.full(termos.size, 0)
-	pares_termos = [(i, j) for j in termos]
-	similaridades_termos = vertex_similarity(g, "salton", pares_termos)
-	return csr_matrix((similaridades_termos, (row, termos)), shape = (1, g.num_vertices()))
-
-
-# somatorio das similaridades entre i e todos os vizinhos de cada anuncio
-def sum_sim(g, anuncios, sim_i_j):
-	row = np.full(anuncios.size, 0)
-	data = []
-	for a in anuncios:
-		vizinhos = g.get_all_neighbors(a)
-		data.append(np.sum(sim_i_j[0, vizinhos]))
-	return csr_matrix((data, (row, anuncios)), shape = (1, g.num_vertices()))
-
 """
 	g = grafo
 	adjMatrix = matriz de adjacências
@@ -28,26 +11,31 @@ def sum_sim(g, anuncios, sim_i_j):
 def msd(g, adjMatrix, partition, i, l):
 	termos = g.get_vertices()[partition.a == 1]
 	anuncios = g.get_vertices()[partition.a == 0]
-	v = []
 
-	similaridades_i_j = sim_i_vertices(g, i, termos)
-	somatorios_alpha = sum_sim(g, anuncios, similaridades_i_j)
-	f_alpha = np.zeros(g.num_vertices())
-	f_alpha[g.get_all_neighbors(i)] = 1
+	f_beta = np.zeros(g.num_vertices()) # o número de anuncios e de termos não é igual ao número de vértices,
+	f_i = np.zeros(g.num_vertices()) # mas isso é feito para facilitar a indexação
 
-	for beta in anuncios:
-		f_i_beta = 0
-		for j in termos:
-			f_i_j = 0
-			for alpha in anuncios:
-				f_i_j += f_alpha[alpha] * ((adjMatrix[i, alpha] * adjMatrix[j, alpha] * similaridades_i_j[0, j]) / somatorios_alpha[0, alpha])
+	anunciosNosTermos = set()
+	vizinhosI = g.get_all_neighbors(i)
 
-			imp_grau_beta = g.get_total_degrees([beta])[0] ** l
-			imp_grau_j = g.get_total_degrees([j])[0] ** (1 - l)
-			
-			f_i_beta += f_i_j * (adjMatrix[j, beta] / (imp_grau_beta * imp_grau_j))
-		v.append((beta, f_i_beta))
-	return v
+	for alpha in vizinhosI:
+		jotas = g.get_all_neighbors(alpha)
+		sim_i_k = np.sum(vertex_similarity(g, "salton", [(i, j) for j in jotas]))
+		
+		for j in jotas:
+			if f_i[j] == 0:
+				anunciosEmJ = g.get_all_neighbors(j)
+				jotasComItensComum = np.intersect1d(vizinhosI, anunciosEmJ, True) #interseção dos vizinhos de j comuns a i
+				sim_i_j = vertex_similarity(g, "salton", [(i, j)])[0]
+
+				f_i[j] = (sim_i_j / sim_i_k) * jotasComItensComum.size
+				anunciosNosTermos |= set(anunciosEmJ)
+
+		for beta in anunciosNosTermos:
+			vizinhos = g.get_all_neighbors(beta)
+			f_beta[beta] = np.sum((1 / ((g.get_total_degrees([beta])[0] ** l) * (g.get_total_degrees(vizinhos) ** (1 - l)))) * f_i[vizinhos])	
+
+	return f_beta
 
 """
 	g = grafo
@@ -86,9 +74,10 @@ def main():
 	g = load_graph("permu-hw7.graphml")
 	_, partition = is_bipartite(g, True)
 	a = adjacency(g)
-	#msd(g, a, partition, 0, 0.55)
 
-	r = md(g, a, partition, 3)
+	r = msd(g, a, partition, 3, 0.55)
+
+	#r = md(g, a, partition, 3)
 
 if __name__ == '__main__':
 	main()
